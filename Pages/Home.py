0931c52cb3168ca472ec1dfdcd1536132c45cc8d
Siddharth Mandgi <siddh30@gmail.com>
2020-05-31 01:00:36 -0400

    Update Home.py

--- Pages/Home.py
+++ Pages/Home.py
@@ -4,5 +4,7 @@ from PIL import Image
 def home():
     image = Image.open('Data/Food.png')
     st.image(image, use_column_width=True)
-    st.success("The app to get you to the closest and the most highly rated places to eat :yum:because guys, hunger is also an emergency! :ambulance: ")
-    st.markdown("- Based on data from TripAdvisor, the app covers restaurants form 20 cities across New York, New Jersey, California, Texas and Washington to recommend the 10 most highly rated restaurants in each state. ")
+    st.warning("The app to get you to the closest and most highly rated places to eat!")
+    st.markdown("Based on data from TripAdvisor, the app covers restaurants form 20 cities across New York, New Jersey, California, Texas and Washington to recommend the 10 most highly rated restaurants in each state. ")
+    st.success("Because hunger is also an emergency!! :ambulance:" ":100:")
+
