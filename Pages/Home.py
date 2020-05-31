90c20265185d806ebebd2b38face17d913977c95
Siddharth  Mandgi <siddharthmandgi@Siddharths-MBP.fios-router.home>
2020-05-30 21:37:42 -0400

    heroku

--- Pages/Home.py
+++ Pages/Home.py
@@ -5,4 +5,4 @@ def home():
     image = Image.open('Data/Food.png')
     st.image(image, use_column_width=True)
     st.success("The app to get you to the closest and the most highly rated places to eat :yum:because guys, hunger is also an emergency! :ambulance: ")
-    st.markdown("- Based on data from TripAdvisor, the app covers restaurants form 20 cities across Ney York, New Jersey, California, Texas and Washnington to recommend the 10 most highly rated restaurants in each state. ")
+    st.markdown("- Based on data from TripAdvisor, the app covers restaurants form 20 cities across New York, New Jersey, California, Texas and Washington to recommend the 10 most highly rated restaurants in each state. ")
--- Pages/Uber.py
+++ Pages/Uber.py
@@ -1,16 +1,22 @@
 import streamlit as st
-import webbrowser
+from bokeh.models.widgets import Div
 
 def funct():
     st.title('Book an Uber or Order from Uber Eats...')
     st.image('Data/Uber.png',use_column_width=500)
     if st.button('Book an Uber'):
-        webbrowser.open('https://www.uber.com/')
+        js = "window.open('https://www.uber.com/')"  # New tab or window
+        js = "window.location.href = 'https://www.uber.com/'"  # Current tab
+        html = '<img src onerror="{}">'.format(js)
+        div = Div(text=html)
+        st.bokeh_chart(div)
+
     st.image('Data/Uber_Eats.png', use_column_width=500)
     if st.button('Order from Uber Eats'):
-        webbrowser.open('https://www.ubereats.com/')
-
-
-
+        js = "window.open('https://www.ubereats.com/')"  # New tab or window
+        js = "window.location.href = 'https://www.ubereats.com/'"  # Current tab
+        html = '<img src onerror="{}">'.format(js)
+        div = Div(text=html)
+        st.bokeh_chart(div)
 
 
